b249ee9a4a269f70ffad17497ff989ac67483c5b
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-03-26 22:12:28 +0700

    update about

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -25,11 +25,11 @@ const About = () => {
         <span className='absolute right-0 bottom-0 z-0 w-60 aspect-square rounded-full bg-blue-600/50 blur-[100px]' />
       </div>
       <div className="container py-10 flex items-start gap-10 border-t border-slate-400/30">
-        <div className="flex-1 flex gap-7 items-start text-lg">
+        <div className="flex-1 flex gap-6 items-start text-lg">
           <div className="w-10 rounded-lg overflow-hidden">
             <img src={profile} alt="hibatillah's logo" className='img-cover' />
           </div>
-          <div className="space-y-4 relative">
+          <div className="flex flex-col gap-4 relative">
             <div className="text-slate-200 font-semibold">
               <h4 className='z-10'>M. Hibatillah Hasanin</h4>
               <h4 className='z-10'>Riau, Indonesia</h4>
@@ -51,8 +51,8 @@ const About = () => {
                 </a>
               </li>
             </ul>
-            <a href={CV} download="CV Hibatillah" className='z-10'>
-              <button className='w-52 px-8 py-2 mt-5 flex flex-nowrap justify-center items-center gap-3 rounded-lg bg-gradient-2 text-lg font-semibold text-white active:brightness-90 overflow-hidden group z-10'>
+            <a href={CV} download="CV Hibatillah" className='w-fit'>
+              <button className='w-52 px-8 py-2 flex flex-nowrap justify-center items-center gap-3 rounded-lg bg-gradient-2 text-lg font-semibold text-white active:brightness-90 overflow-hidden group z-10'>
                 <div className='flex-none flex gap-2 items-center translate-x-[52%] visible opacity-100 transition-all duration-700 ease-in-out group-hover:-translate-x-full group-hover:invisible group-hover:opacity-0'>
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-text-fill" viewBox="0 0 16 16">
                     <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1h-4z"/>
