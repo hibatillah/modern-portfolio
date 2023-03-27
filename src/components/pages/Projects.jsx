19e5448195f3f434fbb94013bc4ebe90dae3dbc9
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-03-28 06:23:33 +0700

    make project card

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import { Heading } from '../utilities'
+import { bookmarks } from '../../db'
+import { ProjectCard } from '../utilities'
 
 const Projects = () => {
   useEffect(() => { document.title = 'Hibatillah - Projects' }, [])
@@ -13,6 +15,15 @@ const Projects = () => {
             highlight="Design"
             paragraph="Discover my projects! Browse through my projects adventures and see the innovative solutions I've developed. Have a look at my projects and get inspired!" 
           />
+      </div>
+      <div className="container py-10 border-t border-slate-400/30 relative">
+        <ul className="grid grid-cols-3 gap-5">
+          {bookmarks.map(item =>
+            <li className='col-span-1 p-5'>
+              <ProjectCard data={item} />
+            </li>
+          )}
+        </ul>
       </div>
     </div>
   )
--- src/components/utilities/Card.jsx
+++ src/components/utilities/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const BookmarkCard = ({data}) => {
   return (
-    <div className='w-full p-5 flex items-start gap-6 rounded-xl bg-glass hover:ring-1 hover:ring-slate-200/50'>
+    <div className='w-full p-5 flex items-start gap-6 rounded-xl bg-slate-50/5 backdrop-blur-lg hover:ring-1 hover:ring-slate-200/10'>
       <div className="flex-none w-12 mb-2">
         <img src={data.img} alt="bookmark's icon" className='img-contain mt-1' />
       </div>
@@ -22,6 +22,11 @@ export const BookmarkCard = ({data}) => {
 
 export const ProjectCard = () => {
   return (
-    <div>Project Card</div>
+    <div className="w-full aspect-square p-4 rounded-3xl bg-slate-800/60 cursor-pointer hover:bg-slate-700/50">
+      <div className="w-full h-4/5 rounded-2xl bg-slate-700/70">
+
+      </div>
+      <h3></h3>
+    </div>
   )
 }
